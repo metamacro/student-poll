db9b5d0378f466bfdb733f785b774a7234a55129
andreiosg <andrej.gardijan@gmail.com>
2020-09-19 17:02:01 +0200

    Added correlation calculation basis, histogram graphing.

--- test/test.py
+++ test/test.py
@@ -1,7 +1,6 @@
 import pandas as pd
-import numpy as np
-
 import matplotlib.pyplot as plt
+from scipy import stats
 
 poll = pd.read_csv('test.csv', sep=r'\s*,\s*',
                    header=0, encoding='ascii', engine='python')
@@ -9,9 +8,23 @@ poll = pd.read_csv('test.csv', sep=r'\s*,\s*',
 print('Internship hourly wage:')
 print('Mean:', poll['intern-hw'].mean())
 print('Median', poll['intern-hw'].median())
+# ddof = 1 since we use a sample not the whole population
 print('Standard deviation:', poll['intern-hw'].std(ddof=1), end=2*'\n')
 
 print('Regular hourly wage:')
 print('Mean:', poll['reg-hw'].mean())
 print('Median', poll['reg-hw'].median())
-print('Standard deviation:', poll['reg-hw'].std(ddof=1))
+print('Standard deviation:', poll['reg-hw'].std(ddof=1), end=2*'\n')
+
+# Spearman's correlation since we don't know the type of correlation
+print('Spearman hourly-regular correlation:', 
+      stats.spearmanr(poll['intern-hw'], poll['reg-hw']))
+
+# visualize the data distribution
+hists = poll[['intern-hw', 'reg-hw']].hist(color='#3377ff')
+
+titles = ['Internship hourly wage [kn]', 'Regular hourly wage [kn]']
+for ax, title in zip(hists.flatten(), titles):
+    ax.title.set_text(title)
+
+plt.show()
